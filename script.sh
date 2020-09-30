76e75a5fc434444094fbdfdd8eb189745a6c6a48
KikicraftMC <kikicraftgamer@gmail.com>
2020-09-30 09:34:57 +0200

    Update script.sh

--- script.sh
+++ script.sh
@@ -1,7 +1,7 @@
 clear
 echo    
 printf "      \e[1;77m\e[41m  Obtén toda la lista de usuarios de tu sistema!  \e[0m\n"
-echo "         Lista de usuarios y carpeta raiz de cada uno."
+echo "                    Lista de usuarios y carpeta raiz de cada uno."
 echo    
 echo   
 read -p "Pulsa ENTER para comenzar"
@@ -13,8 +13,10 @@ sleep 2
 echo Terminando proceso de obtención, espere...
 sleep 2
 cat /etc/passwd
+echo
+echo
 read -p "Si has terminado, precione ENTER."
 clear
-echo Gilipollas!
-pause 2
+echo Gilipolla !
+pause 1
 exit
