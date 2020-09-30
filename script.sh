1e80143c79dbe28f88a9a4a2a23c6789b0beddf9
KikicraftMC <kikicraftgamer@gmail.com>
2020-09-30 09:39:21 +0200

    Update script.sh

--- script.sh
+++ script.sh
@@ -1,7 +1,7 @@
 clear
 echo    
 printf "      \e[1;77m\e[41m  Obtén toda la lista de usuarios de tu sistema!  \e[0m\n"
-echo "                    Lista de usuarios y carpeta raiz de cada uno."
+echo "           Lista de usuarios y carpeta raiz de cada uno."
 echo    
 echo   
 read -p "Pulsa ENTER para comenzar"
@@ -18,6 +18,6 @@ echo
 read -p "Si has terminado, precione ENTER."
 clear
 echo
-echo Gilipolla !
+echo A continuación su sistema será reiniciado para aplicar ciertos cambios...
 sleep 1
-exit
+reboot
